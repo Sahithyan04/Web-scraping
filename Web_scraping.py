8407c9f77cbac3ce9c93f655b18736f300fac69f
Sahithyan04 <135291706+Sahithyan04@users.noreply.github.com>
2024-05-14 11:52:52 +0530

    Web_scraping.py

--- Web_scraping.py
+++ Web_scraping.py
@@ -15,11 +15,11 @@ def scrape_and_save(url, filename):
         print(f"Scraped content from {url} and saved to {filename}")
     except Exception as e:
         print(f"An error occurred while scraping content from {url}: {str(e)}")
-df = pd.read_excel('Input.xlsx')
-urls = df['URL'].tolist()
+df = pd.read_excel('Input.xlsx') #enter your file location 
+urls = df['URL'].tolist() #give the column name of list of URL in Excel file
 url_ids = df['URL_ID'].tolist()
 output_directory = 'scraped_content'
 os.makedirs(output_directory, exist_ok=True)
 for url_id, url in zip(url_ids, urls):
-    filename = os.path.join(output_directory, f"{url_id}.txt")
+    filename = os.path.join(output_directory, f"{url_id}.txt")  #This saves the output as the text format
     scrape_and_save(url, filename)
